9f2ce34e7b58aabbf6bcceb8249d3d86e06b0798
Diego Bermudez <158313944+diegobermudez03@users.noreply.github.com>
2024-05-16 00:16:33 -0500

    Pictures uploaded to pinterest so the images are internet URL's

--- src/view/auth/styles/SigninPageWrapper.js
+++ src/view/auth/styles/SigninPageWrapper.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Wrapper from 'view/auth/styles/Wrapper';
 
 const SigninPageWrapper = styled(Wrapper)`
-  background-image: url(/images/signin.jpg);
+  background-image: url('https://i.pinimg.com/originals/9c/21/6d/9c216d6fee8f48d963204769491ed4d6.jpg');
 `;
 
 export default SigninPageWrapper;
--- src/view/auth/styles/SignupPageWrapper.js
+++ src/view/auth/styles/SignupPageWrapper.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import Wrapper from 'view/auth/styles/Wrapper';
 
 const SignupPageWrapper = styled(Wrapper)`
-  background-image: url(/images/signup.jpg);
+  background-image: url('https://i.pinimg.com/originals/fa/c5/82/fac58203675e96e431c72cc62aed9db3.jpg');
 `;
 
 export default SignupPageWrapper;
